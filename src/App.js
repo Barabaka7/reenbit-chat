29d0efb91a21b58c1a3e7a170fc148b50cbad7fd
Maksym Hopei <maksym.hopei@jsmd-group.com>
2022-08-24 16:31:16 +0200

    Fix saving state to local storage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
-import { UserPane } from "./containers/userPane/UserPane.js"
-import { CurrentChatPane } from "./containers/currentChatPane/CurrentChatPane.js"
-import { CONTACTS } from "./Utilities/data.js"
+import React, {useEffect, useState} from "react";
+import {UserPane} from "./containers/userPane/UserPane.js"
+import {CurrentChatPane} from "./containers/currentChatPane/CurrentChatPane.js"
+import {CONTACTS} from "./Utilities/data.js"
 import GoogleLogin from 'react-google-login';
 
-function App() {
+function App({test}) {
 
   const [currentChat, setCurrentChat] = useState(1);
-  const [contacts, setContacts] = useState(CONTACTS);
+  const [contacts, setContacts] = useState([]);
 
-  useEffect(()=>{
-   // setContacts(CONTACTS);
-//setContacts(JSON.parse(window.localStorage.getItem('contacts')));
-    //setCurrentChat(JSON.parse(window.localStorage.getItem('currentChat')));
- // alert(`alert from mounting ${JSON.parse(window.localStorage.getItem('contacts'))}`);
+  useEffect(() => {
+    setContacts(JSON.parse(window.localStorage.getItem('contacts')) || CONTACTS);
+    setCurrentChat(parseInt(window.localStorage.getItem('currentChat')) || 1);
+    // alert(`alert from mounting ${JSON.parse(window.localStorage.getItem('contacts'))}`);
   }, [])
 
-  useEffect(()=>{
-  //  window.localStorage.setItem('contacts', contacts);
-    //alert(`alert from current chat ${window.localStorage.getItem('contacts')}`)
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    // console.log(`alert from current chat ${window.localStorage.getItem('contacts')}`)
   }, [contacts])
 
-  useEffect(()=>{
-  //  window.localStorage.setItem('currentChat', currentChat);
-  //  alert(`alert from current chat ${window.localStorage.getItem('currentChat')}`)
+  useEffect(() => {
+    window.localStorage.setItem('currentChat', currentChat.toString());
+    //  alert(`alert from current chat ${window.localStorage.getItem('currentChat')}`)
   }, [currentChat])
 
   const addNewMessage = (newMessage, from, contactId) => {
@@ -35,38 +34,40 @@ function App() {
       from: from,
       unread: false
     };
-   
+
     setContacts(prev => {
-          const new_contacts = prev.map( 
-              contact => {
-                   if (contact.contactId === contactId) {             
-             return {...contact, messages: [...contact.messages, addingMesssage]}
-            }
-                   else {return contact}})
-            return new_contacts;
+      const new_contacts = prev.map(
+        contact => {
+          if (contact.contactId === contactId) {
+            return {...contact, messages: [...contact.messages, addingMesssage]}
+          } else {
+            return contact
+          }
+        })
+      return new_contacts;
     })
-    }
+  }
 
   return (
-    <div className="App">  
-    <header>
-      <h1>Google Login</h1>
-    <div><GoogleLogin 
-      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-      buttonText="Log in with Google"/>
-    </div>
-    </header>
-       
-       <div className="leftContainer">
-          <UserPane contacts={ contacts }
-                    setCurrentChat={setCurrentChat}/>
+    <div className="App">
+      <header>
+        <h1>Google Login</h1>
+        <div><GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          buttonText="Log in with Google" />
+        </div>
+      </header>
+
+      <div className="leftContainer">
+        <UserPane contacts={contacts}
+                  setCurrentChat={setCurrentChat} />
       </div>
       <div className="rightContainer">
-          <CurrentChatPane
-                           contacts={contacts}
-                           currentChat={currentChat}
-                           addNewMessage={addNewMessage} /> 
-        </div>
+        <CurrentChatPane
+          contacts={contacts}
+          currentChat={currentChat}
+          addNewMessage={addNewMessage} />
+      </div>
     </div>
   );
 }
