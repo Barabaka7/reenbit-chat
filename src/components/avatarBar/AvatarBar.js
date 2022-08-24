109cd38820a1275a5489740c60b27383e3349c36
Maksym Hopei <maksym.hopei@jsmd-group.com>
2022-08-24 16:30:08 +0200

    Fix controlled/uncontrolled state change in AvatarBar

--- src/components/avatarBar/AvatarBar.js
+++ src/components/avatarBar/AvatarBar.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 export const AvatarBar = ({avatarData}) => {
-
-  const statusOnline =  <input type="checkbox" checked disabled/>
-  const statusOffline = <input type="checkbox" disabled/>
-  const status = avatarData.isOnline ? statusOnline : statusOffline;
-
-    return (
+  return (
     <div className="avatar">
-       <img src={avatarData.avatar} className="avatar" /> 
-       {status}
-       <span className="contactName">{avatarData.contactName}</span>
+      <img src={avatarData.avatar} className="avatar" />
+      <input type="checkbox" disabled checked={avatarData.isOnline} />
+      <span className="contactName">{avatarData.contactName}</span>
     </div>
-
   );
-    };
+};
